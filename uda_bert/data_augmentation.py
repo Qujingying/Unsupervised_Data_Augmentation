95c1123a3919a2ecf0729671f3cf3e23c78995e1
bhacquin <bastien.van-delft@unchartech.com>
2019-05-28 19:28:07 +0200

    Debug

--- uda_bert/data_augmentation.py
+++ uda_bert/data_augmentation.py
@@ -211,7 +211,7 @@ def convert_examplesUDA_to_features(examples, max_seq_length,
 #   label_map = {label : i for i, label in enumerate(label_list)}
 
   features = []
-  for (ex_index, (example,example_2)) in enumerate(examples):
+  for ex_index, (example,example_2) in enumerate(examples):
       if ex_index % 10000 == 0:
           logger.info("Writing example %d of %d" % (ex_index, len(examples)))
 
