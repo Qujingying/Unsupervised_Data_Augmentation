3f86ff6a20e766885e794552eae059a319ef488e
bhacquin <bastien.van-delft@unchartech.com>
2019-05-29 10:42:32 +0200

    Debug

--- uda_bert/data_augmentation.py
+++ uda_bert/data_augmentation.py
@@ -173,7 +173,7 @@ def prepare_with_back_translate(text, translator, selected_lang, target_lang, ep
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
     for doc_idx in trange(len(docs), desc="Document"):
         print(docs[doc_idx])
-        instance = convert_examplesUDA_to_features(docs[doc_idx],max_seq_length = 512, tokenizer = tokenizer,output_mode = "UDA"))
+        instance = convert_examplesUDA_to_features(docs[doc_idx],max_seq_length = 512, tokenizer = tokenizer,output_mode = "UDA")
         docs_instances_unsup.append(instance)
     docs_instances_unsup = np.array(docs_instances_unsup).T
     pickle.dump(docs_instances_unsup,open('data_unsup.p','wb'))
